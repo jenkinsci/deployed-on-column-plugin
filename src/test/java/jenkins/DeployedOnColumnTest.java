5156b496db617194d2f7abe68ada6652cbf68654
Baptiste Mathus <batmat@batmat.net>
2022-02-01 17:55:43 +0100

    Adjust fake test...

--- src/test/java/jenkins/DeployedOnColumnTest.java
+++ src/test/java/jenkins/DeployedOnColumnTest.java
@@ -7,7 +7,7 @@ import jenkins.plugins.deployedoncolumn.DeployedOnColumn;
 
 public class DeployedOnColumnTest {
     @Test
-    public void isAbsolute() {
-        Assert.assertTrue(new DeployedOnColumn().isAbsolute("/"));
+    public void isAbsoluteDummy() {
+        Assert.assertFalse(new DeployedOnColumn().isAbsolute("seriously"));
     }
 }
